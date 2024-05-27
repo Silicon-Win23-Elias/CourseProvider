bfd96bbef9234c5fabeab4428f9a4c13875ef8a7
lundqvisthampus <lundqvisthampus@outlook.com>
2024-05-27 15:00:07 +0200

    JWT authorization finished

--- CourseProvider/Functions/GraphQL.cs
+++ CourseProvider/Functions/GraphQL.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CourseProvider.Functions
 {
@@ -18,7 +22,47 @@ namespace CourseProvider.Functions
                 return new UnauthorizedResult();
             }
 
+
+            var token = authHeader.FirstOrDefault()?.Split(" ").Last();
+            if (string.IsNullOrEmpty(token))
+            {
+                return new UnauthorizedResult();
+            }
+
+            if (!ValidateToken(token, out ClaimsPrincipal claimsPrincipal))
+            {
+                return new UnauthorizedResult();
+            }
+
+
             return await _graphQLRequestExecutor.ExecuteAsync(req);
+        }
+
+        private static bool ValidateToken(string token, out ClaimsPrincipal claimsPrincipal)
+        {
+            claimsPrincipal = null;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("3e0f2db8-3ce3-4e22-b53a-7a609b4b7048");
+
+            try
+            {
+                claimsPrincipal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = "TokenProvider",
+                    ValidateAudience = true,
+                    ValidAudience = "Silicon",
+                    ValidateLifetime = true,
+                }, out SecurityToken validatedToken);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
